01ff010ff0ba962a8eb548e28aaac4f22ba9b7c6
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 12:11:44 -0700

    Create connection handler for Socket.io app - Application sends the user a welcome message upon connection - Waits for user to send a message which is then sent to all sockets

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ app.get("/", function(req, res){
 app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
