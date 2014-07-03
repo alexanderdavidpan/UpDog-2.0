5b791c9a99d9366a9eb5e0d1597724cf1542d360
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 12:08:39 -0700

    Add Socket.io integration - Pass ExpressJS server to Socket.io

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ app.get("/", function(req, res){
 });
  
 app.use(express.static(__dirname + '/public'));
-app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
