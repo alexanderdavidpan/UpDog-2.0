abe4f07bf62ea5bf9dcb0536a826305073d794fc
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 11:57:48 -0700

    Set Jade template files in Express - Also inform Express to use Jade template engine

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ app.get("/", function(req, res){
  
 app.listen(port);
 console.log("Listening on port " + port);
+
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
