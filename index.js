598589dfe686f5092f3949cb6715f05f66ff54da
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 13:20:05 -0700

    Remove accidental duplication of Chat node project - Accidently cloned project into the same folder =X

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 var express = require("express");
 var app = express();
 var port = 3700;
- 
-app.get("/", function(req, res){
-    res.send("Welcome to UpDog!");
-});
- 
-app.use(express.static(__dirname + '/public'));
-var io = require('socket.io').listen(app.listen(port));
-console.log("Listening on port " + port);
 
 app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res){
     res.render("page");
 });
+
+
+var io = require('socket.io').listen(app.listen(port));
 
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
@@ -23,3 +20,4 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('message', data);
     });
 });
+console.log("Listening on port " + port);
