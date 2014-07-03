4cbf047ac2ce894345ddb7bef9f7aea7f5e68ae3
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 12:04:05 -0700

    Create public directory for external JS - Link app to external JS public directory

--- index.js
+++ index.js
@@ -3,15 +3,9 @@ var app = express();
 var port = 3700;
  
 app.get("/", function(req, res){
-    res.send("Welcome to Updog!");
+    res.send("Welcome to UpDog!");
 });
  
+app.use(express.static(__dirname + '/public'));
 app.listen(port);
 console.log("Listening on port " + port);
-
-app.set('views', __dirname + '/tpl');
-app.set('view engine', "jade");
-app.engine('jade', require('jade').__express);
-app.get("/", function(req, res){
-    res.render("page");
-});
