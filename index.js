ecbfd14c223acb1c832480f0a3c73b27d099d68f
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-03 12:41:56 -0700

    Add necessary JS files to page.jade - Link socket.io and chat.js files to page.jade

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+app.set('views', __dirname + '/tpl');
+app.set('view engine', "jade");
+app.engine('jade', require('jade').__express);
+app.get("/", function(req, res){
+    res.render("page");
+});
+
 io.sockets.on('connection', function (socket) {
     socket.emit('message', { message: 'welcome to the chat' });
     socket.on('send', function (data) {
