9a3a48051776df3546b66823e4c89c98b0ee4e28
Alexander Pan <alexanderdavidpan@gmail.com>
2014-07-25 19:07:48 -0700

    Reindent express attributes - One line had wierd indentation

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 //Create new express server
 var express = require('express') 
-  ,app = express()
+    ,app = express()
     ,http = require('http')
     ,server = http.createServer(app)
     ,io = require('socket.io').listen(server)
